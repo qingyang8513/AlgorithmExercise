472ece6236f20807c1d016688e31cf0eebe4423e
qingyang8513 <majinbuaa@163.com>
2020-09-21 22:22:38 +0800

    Init the repo
    None

---
+++ src/main.cc
@@ -0,0 +1,21 @@
+// Copyright 2020 MTRobot.
+// Created by Martin on 2020.0801
+
+#include <iostream>
+
+static void ShowUsage(void) {
+  std::cout << "===========================================================\n";
+	std::cout << "AlgorithmExercise\n";
+	std::cout << "===========================================================\n";
+	std::cout << "Usage: ./exe [cmd]\n";
+	std::cout << "===========================================================\n";
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    ShowUsage();
+    return 0;
+  }
+
+  return 0;
+}
